c2f1213f3584aa458053f3297d252575f00d1eef
lailabougria <laila.bougria@particular.net>
2020-10-01 16:28:03 +0200

    Add persistence tests

---
+++ src/NServiceBus.Persistence.NonDurable.PersistenceTests/PersistenceTestsConfiguration.cs
@@ -0,0 +1,45 @@
+﻿namespace NServiceBus.PersistenceTesting
+{
+    using System.Threading.Tasks;
+    using NServiceBus;
+    using NServiceBus.Outbox;
+    using NServiceBus.Sagas;
+    using Persistence;
+
+    public partial class PersistenceTestsConfiguration
+    {
+        public bool SupportsDtc => true;
+
+        public bool SupportsOutbox => true;
+
+        public bool SupportsFinders => false;
+
+        public bool SupportsPessimisticConcurrency => false;
+
+        public ISagaIdGenerator SagaIdGenerator { get; private set; }
+
+        public ISagaPersister SagaStorage  { get; private set; }
+
+        public ISynchronizedStorage SynchronizedStorage { get; private set; }
+
+        public ISynchronizedStorageAdapter SynchronizedStorageAdapter  { get; private set; }
+
+        public IOutboxStorage OutboxStorage  { get; private set; }
+
+        public Task Configure()
+        {
+            SagaIdGenerator = new DefaultSagaIdGenerator();
+            SagaStorage = new NonDurableSagaPersister();
+            SynchronizedStorage = new NonDurableSynchronizedStorage();
+            SynchronizedStorageAdapter = new NonDurableTransactionalSynchronizedStorageAdapter();
+            OutboxStorage = new NonDurableOutboxStorage();
+
+            return Task.CompletedTask;
+        }
+
+        public Task Cleanup()
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
