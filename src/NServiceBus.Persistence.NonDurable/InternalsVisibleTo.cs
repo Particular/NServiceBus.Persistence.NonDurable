89abebf944f8b8f34a1953fd736e553a9366af6f
lailabougria <laila.bougria@particular.net>
2020-10-01 17:15:37 +0200

    Moved over unit tests that were in core

--- src/NServiceBus.Persistence.NonDurable/InternalsVisibleTo.cs
+++ src/NServiceBus.Persistence.NonDurable/InternalsVisibleTo.cs
@@ -1,3 +1,4 @@
 ﻿using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("NServiceBus.Persistence.NonDurable.PersistenceTests")]
+[assembly: InternalsVisibleTo("NServiceBus.Persistence.NonDurable.Tests")]
