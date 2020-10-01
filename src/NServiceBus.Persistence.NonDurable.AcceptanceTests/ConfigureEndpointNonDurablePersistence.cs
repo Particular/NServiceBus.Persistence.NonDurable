0d8c9939667b04bbb3af5d91de5225aebac1a0b4
lailabougria <laila.bougria@particular.net>
2020-10-01 16:48:42 +0200

    Add acceptance tests

---
+++ src/NServiceBus.Persistence.NonDurable.AcceptanceTests/ConfigureEndpointNonDurablePersistence.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+using NServiceBus.AcceptanceTesting.Support;
+
+namespace NServiceBus.AcceptanceTests
+{
+    public class ConfigureEndpointNonDurablePersistence : IConfigureEndpointTestExecution
+    {
+        public Task Configure(string endpointName, EndpointConfiguration configuration, RunSettings settings, PublisherMetadata publisherMetadata)
+        {
+            configuration.UsePersistence<InMemoryPersistence>();
+            return Task.FromResult(0);
+        }
+
+        public Task Cleanup()
+        {
+            // Nothing required for non-durable persistence
+            return Task.FromResult(0);
+        }
+    }
+}
