0647c7604ecf77e4d70673143e42a57d2d92d266
Tim Bussmann <timbussmann@users.noreply.github.com>
2022-03-02 18:17:45 +0100

    make assembly scanning work

---
+++ src/NServiceBus.Persistence.NonDurable.AcceptanceTests/NonDurableTestSetup.cs
@@ -0,0 +1,12 @@
+﻿using NServiceBus;
+using NUnit.Framework;
+
+[SetUpFixture]
+public class NonDurableTestSetup
+{
+    [OneTimeSetUp]
+    public void SetupFixture()
+    {
+        typeof(NonDurablePersistence).ToString();
+    }
+}
