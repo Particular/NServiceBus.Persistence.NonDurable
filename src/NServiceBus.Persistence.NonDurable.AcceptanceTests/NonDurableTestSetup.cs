fe40e97bb79ad1bb32141e2762b87de724e571de
Mike Minutillo <mike.minutillo@particular.net>
2022-03-03 12:03:39 +0800

    Added explanation comment

--- src/NServiceBus.Persistence.NonDurable.AcceptanceTests/NonDurableTestSetup.cs
+++ src/NServiceBus.Persistence.NonDurable.AcceptanceTests/NonDurableTestSetup.cs
@@ -1,6 +1,8 @@
 ﻿using NServiceBus;
 using NUnit.Framework;
 
+// HINT: Workaround for https://github.com/Particular/NServiceBus/pull/6291
+// Ensures that the NonDurablePersistence assembly has been loaded into the AppDomain
 [SetUpFixture]
 public class NonDurableTestSetup
 {
