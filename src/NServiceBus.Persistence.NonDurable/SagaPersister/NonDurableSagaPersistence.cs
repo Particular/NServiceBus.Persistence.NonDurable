766ade6ec08e0789be9dbe4bb3d29435e90e4e4a
Daniel Marbach <daniel.marbach@openplace.net>
2022-05-10 15:51:08 +0200

    More feature alignment

--- src/NServiceBus.Persistence.NonDurable/SagaPersister/NonDurableSagaPersistence.cs
+++ src/NServiceBus.Persistence.NonDurable/SagaPersister/NonDurableSagaPersistence.cs
@@ -7,12 +7,13 @@
     {
         public NonDurableSagaPersistence()
         {
+            Defaults(s => s.EnableFeatureByDefault<NonDurableTransactionalStorageFeature>());
+
             DependsOn<Sagas>();
+            DependsOn<NonDurableTransactionalStorageFeature>();
         }
 
-        protected override void Setup(FeatureConfigurationContext context)
-        {
+        protected override void Setup(FeatureConfigurationContext context) =>
             context.Services.AddSingleton<ISagaPersister, NonDurableSagaPersister>();
-        }
     }
 }
---
+++ src/NServiceBus.Persistence.NonDurable/SynchronizedStorage/NonDurableTransactionalStorageFeature.cs
@@ -0,0 +1,16 @@
+﻿namespace NServiceBus
+{
+    using Microsoft.Extensions.DependencyInjection;
+    using Persistence;
+    using Features;
+
+    class NonDurableTransactionalStorageFeature : Feature
+    {
+        public NonDurableTransactionalStorageFeature()
+        {
+            DependsOn<SynchronizedStorage>();
+        }
+        protected override void Setup(FeatureConfigurationContext context) =>
+            context.Services.AddScoped<ICompletableSynchronizedStorageSession, NonDurableSynchronizedStorageSession>();
+    }
+}
