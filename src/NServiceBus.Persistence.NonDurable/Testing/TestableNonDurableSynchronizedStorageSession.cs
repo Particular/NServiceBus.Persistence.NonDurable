b4f6a47f66ca22a1482250a99a0ef2a36b809915
Daniel Marbach <danielmarbach@users.noreply.github.com>
2026-06-30 20:00:31 +0200

    Provide ability to specify options and remove unnecessary generic

--- src/NServiceBus.Persistence.NonDurable/Testing/TestableNonDurableSynchronizedStorageSession.cs
+++ src/NServiceBus.Persistence.NonDurable/Testing/TestableNonDurableSynchronizedStorageSession.cs
@@ -11,27 +11,40 @@ using Persistence.NonDurable.SagaPersister;
 /// A fake implementation of the NonDurable synchronized storage session for testing purposes.
 /// </summary>
 /// <remarks>
-/// Initializes a new instance of <see cref="TestableNonDurableSynchronizedStorageSession" /> using the specified storage instance.
+/// Initializes a new instance of <see cref="TestableNonDurableSynchronizedStorageSession" /> using the specified storage instance and saga options.
 /// </remarks>
-public class TestableNonDurableSynchronizedStorageSession(NonDurableStorage storage) : ISynchronizedStorageSession, INonDurableStorageSession
+public class TestableNonDurableSynchronizedStorageSession(NonDurableStorage storage, NonDurableSagaOptions sagaOptions) : ISynchronizedStorageSession, INonDurableStorageSession
 {
     /// <summary>
-    /// Initializes a new instance of <see cref="TestableNonDurableSynchronizedStorageSession" /> using a new <see cref="NonDurableStorage" /> instance.
+    /// Initializes a new instance of <see cref="TestableNonDurableSynchronizedStorageSession" /> using a new <see cref="NonDurableStorage" /> instance and default saga options.
     /// </summary>
-    public TestableNonDurableSynchronizedStorageSession() : this(new NonDurableStorage())
+    public TestableNonDurableSynchronizedStorageSession() : this(new NonDurableStorage(), new NonDurableSagaOptions())
+    {
+    }
+
+    /// <summary>
+    /// Initializes a new instance of <see cref="TestableNonDurableSynchronizedStorageSession" /> using a new <see cref="NonDurableStorage" /> instance and the specified saga options.
+    /// </summary>
+    public TestableNonDurableSynchronizedStorageSession(NonDurableSagaOptions sagaOptions) : this(new NonDurableStorage(), sagaOptions)
+    {
+    }
+
+    /// <summary>
+    /// Initializes a new instance of <see cref="TestableNonDurableSynchronizedStorageSession" /> using the specified storage instance and default saga options.
+    /// </summary>
+    public TestableNonDurableSynchronizedStorageSession(NonDurableStorage storage) : this(storage, new NonDurableSagaOptions())
     {
     }
 
     /// <summary>
     /// Adds saga data to the test session storage.
     /// </summary>
-    public void AddSaga<TSagaData>(TSagaData sagaData)
-        where TSagaData : class, IContainSagaData
+    public void AddSaga(IContainSagaData sagaData)
     {
         ArgumentNullException.ThrowIfNull(sagaData);
 
         var noCorrelationId = new CorrelationId(typeof(object), string.Empty, new object());
-        storage.Sagas[sagaData.Id] = new SagaEntry(sagaData, noCorrelationId, version: 1, new NonDurableSagaOptions().JsonSerializerOptions);
+        storage.Sagas[sagaData.Id] = new SagaEntry(sagaData, noCorrelationId, version: 1, sagaOptions.JsonSerializerOptions);
     }
 
     /// <inheritdoc />
@@ -43,4 +56,7 @@ public class TestableNonDurableSynchronizedStorageSession(NonDurableStorage stor
     public TSagaData? GetSagaData<TSagaData, TState>(IReadOnlyContextBag context, TState state, Func<TSagaData, TState, bool> predicate, CancellationToken cancellationToken = default)
         where TSagaData : class, IContainSagaData =>
         NonDurableSagaDataProjection.GetSagaData(storage, context, state, predicate, cancellationToken);
+
+    readonly NonDurableStorage storage = storage ?? throw new ArgumentNullException(nameof(storage));
+    readonly NonDurableSagaOptions sagaOptions = sagaOptions ?? throw new ArgumentNullException(nameof(sagaOptions));
 }
