620d465792d45cf3ff965ea729dcdc3886782e7d
David Boike <david.boike@gmail.com>
2026-07-21 10:01:59 -0500

    Exception message formatting

--- src/NServiceBus.Persistence.NonDurable/SagaPersister/NonDurableSagaLockTimeoutException.cs
+++ src/NServiceBus.Persistence.NonDurable/SagaPersister/NonDurableSagaLockTimeoutException.cs
@@ -3,6 +3,6 @@ namespace NServiceBus.Persistence.NonDurable.SagaPersister;
 using System;
 
 sealed class NonDurableSagaLockTimeoutException(Guid sagaId, TimeSpan timeout)
-    : TimeoutException($"NonDurableSagaPersister timed out after waiting {timeout} to acquire saga entity Id[{sagaId}].")
+    : TimeoutException($"NonDurableSagaPersister timed out after waiting {timeout} to acquire saga entity Id {sagaId}.")
 {
 }
