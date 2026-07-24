4188150013ff957d163e2b6d33c901155ce13dd8
Daniel Marbach <danielmarbach@users.noreply.github.com>
2026-07-24 16:41:29 +0200

    Await asynchronous saga projection

--- src/NServiceBus.Persistence.NonDurable.AcceptanceTests/When_using_the_synchronized_storage_session_to_find_saga_data.cs
+++ src/NServiceBus.Persistence.NonDurable.AcceptanceTests/When_using_the_synchronized_storage_session_to_find_saga_data.cs
@@ -78,10 +78,10 @@ public class When_using_the_synchronized_storage_session_to_find_saga_data : NSe
 
     public class ProcessExecutionSagaFinder : ISagaFinder<ProcessExecutionSagaData, ContinueWithServerTask>
     {
-        public Task<ProcessExecutionSagaData> FindBy(ContinueWithServerTask message,
+        public async Task<ProcessExecutionSagaData> FindBy(ContinueWithServerTask message,
             ISynchronizedStorageSession storageSession, IReadOnlyContextBag context,
             CancellationToken cancellationToken = default) =>
-            storageSession.NonDurablePersistenceSession().FindSagaData<ProcessExecutionSagaData, Guid>(
+            await storageSession.NonDurablePersistenceSession().FindSagaData<ProcessExecutionSagaData, Guid>(
                 context,
                 message.ServerTaskId,
                 static (sagaData, serverTaskId) => sagaData.ServerTaskId == serverTaskId,
